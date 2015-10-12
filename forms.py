57fa14ad4446f94b65bc3052b6f52c49be477e4f
Adam Schaal <amschaal@ucdavis.edu>
2015-10-12 14:38:07 -0700

    work on layouts, etc

--- forms.py
+++ forms.py
@@ -8,6 +8,8 @@ from django.core.urlresolvers import reverse
 from django.utils.html import format_html
 from django.conf import settings
 from models import Response
+from crispy_forms.layout import HTML, Fieldset, Layout
+from crispy_forms.helper import FormHelper
 UPLOAD_DIRECTORY = getattr(settings,'DJANGO_JSON_FORMS_UPLOAD_DIRECTORY')
 GET_UPLOAD_PATH = getattr(settings,'DJANGO_JSON_FORMS_GET_UPLOAD_PATH',False)
 from django.utils.module_loading import import_string
@@ -19,6 +21,10 @@ class JSONModelForm(forms.Form):
         super(JSONModelForm, self).__init__(*args, **kwargs)
         fh = FieldHandler(self.json_form_model.fields)
         self.fields = fh.formfields
+        lh = LayoutHandler(self.json_form_model.fields)
+        self.helper = FormHelper()
+        self.helper.layout = lh.layout
+        self.helper.form_tag = False
     def create_response(self):
         if self.is_valid():
             if not hasattr(self, 'response'):
@@ -45,11 +51,20 @@ class JSONModelForm(forms.Form):
 
 class JSONForm(forms.Form):
     def __init__(self,*args,**kwargs):
-        fields = json.loads(kwargs.pop('fields','[]'))
+#         print kwargs
+#         try:
+#             fields = json.loads(kwargs.pop('fields','[]'))
+#         except:
+        fields = kwargs.pop('fields','[]')
+        if not isinstance(fields,list):
+            raise Exception('fields argument must be a list')
         super(JSONForm, self).__init__(*args, **kwargs)
         fh = FieldHandler(fields)
         self.fields = fh.formfields
-        
+        lh = LayoutHandler(fields)
+        self.helper = FormHelper()
+        self.helper.layout = lh.layout
+        self.helper.form_tag = False
 class JSONFormModel_Form(forms.ModelForm):
     def __init__(self,*args,**kwargs):
         super(JSONFormModel_Form, self).__init__(*args, **kwargs)
@@ -60,13 +75,30 @@ class JSONFormModel_Form(forms.ModelForm):
         model=JSONFormModel
         fields = ('name','description','fields')
 
+
+class LayoutHandler():
+    def __init__(self, fields):
+        self.layout_items = []
+#         print fields
+        for field in fields:
+            if field.has_key('type'):
+                print field['type']
+                if field['type'] == 'layout_html':
+                    self.layout_items.append(HTML(field['html']))
+                else:
+                    self.layout_items.append(field['name'])
+        print self.layout_items
+        self.layout = Layout(*self.layout_items)
+
 class FieldHandler():
     def __init__(self, fields):
         self.formfields = {}
         for field in fields:
-            options = self.get_options(field)
-            f = getattr(self, "create_field_for_"+field['type'] )(field, options)
-            self.formfields[field['name']] = f
+            if field.has_key('type'):
+                if hasattr(self, "create_field_for_"+field['type']):
+                    options = self.get_options(field)
+                    f = getattr(self, "create_field_for_"+field['type'] )(field, options)
+                    self.formfields[field['name']] = f
 
     def get_options(self, field):
         options = {}
---
+++ static/django_json_forms/scripts/designer.js
@@ -0,0 +1,54 @@
+var DesignerApp = angular.module('DesignerApp', ['ui.tinymce'])
+.config(function($interpolateProvider) {
+	  $interpolateProvider.startSymbol('{[');
+	  $interpolateProvider.endSymbol(']}');
+});
+
+DesignerApp.controller('DesignerController', function($scope,$http) {
+    $scope.field_types = [
+                          {'class':'field','type':'text', 'label': 'Text Field'},
+                          {'class':'field','type':'textarea', 'label': 'TextArea Field'},
+                          {'class':'field','type':'file', 'label': 'File Field'},
+                          {'class':'field','type':'radio', 'label': 'Radio Field'},
+                          {'class':'field','type':'integer', 'label': 'Integer Field'},
+                          {'class':'field','type':'select', 'label': 'Select Field'},
+                          {'class':'field','type':'checkbox', 'label': 'Checkbox Field'},
+                          {'class':'layout','type':'layout_html', 'label': 'HTML Label'}
+                          ];
+    
+    $scope.fields=[];
+    $scope.initialize = function(init){
+    	if (init.fields)
+    		$scope.fields = init.fields;
+    	if (init.id)
+    		$scope.id = init.id;
+    	$scope.urls = init.urls;
+    };
+    $scope.move = function(index,diff){
+    	$scope.fields.splice(index+diff, 0, angular.copy($scope.fields.splice(index, 1)[0]));
+    };
+    $scope.save = function(){
+    	console.log('posting: ',$scope.id, $scope.fields);
+    	$http.post($scope.urls.update,{fields:$scope.fields})
+    	.success(function(){alert('The form has been updated.')})
+    	.error(function(){alert('There was an error updating the form.')});
+    };
+    $scope.addField = function(){
+    	$scope.fields.push({});
+    };
+    $scope.test = function(){
+    	console.log($scope.fields);
+    }
+    $scope.addOption = function(field){
+    	if (!field.choices)
+    		field.choices = [];
+    	field.choices.push({});
+    }
+    $scope.removeOption = function(field,index){
+    	field.choices.splice(index,1);
+    }
+    $scope.removeField = function(index){
+    	if(confirm('Are you sure you want to remove this field?'))
+    		$scope.fields.splice(index,1);
+    }
+  });
